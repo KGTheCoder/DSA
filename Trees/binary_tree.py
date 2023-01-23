6549c8e4adc49b9f339525e93636a72174f2c254
KG <kgitts98@gmail.com>
2023-01-23 10:58:14 -0800

    .

--- Trees/binary_tree.py
+++ Trees/binary_tree.py
@@ -23,5 +23,26 @@ class Tree:
           if current is None:
             parent.left_child = node
             return self.root_node
-          else:
-            current = current.right_child
+        else:
+          current = current.right_child
+          if current is None:
+            parent.right_child = node
+            return self.root_node
+  
+  def inorder(self, root_node):
+    current = root_node
+    if current is None:
+      return
+    self.inorder(current.left_child)
+    print(current.data)
+    self.inorder(current.right_child)
+
+
+tree = Tree()
+r = tree.insert(5)
+r = tree.insert(2)
+r = tree.insert(7)
+r = tree.insert(9)
+r = tree.insert(1)
+
+print(tree.inorder(r))
