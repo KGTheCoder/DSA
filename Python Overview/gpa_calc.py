37e996ef66921d5e35d3bbc432556a61106abd2b
KG <kgitts98@gmail.com>
2023-01-24 09:47:40 -0800

    .

--- Python Overview/gpa_calc.py
+++ Python Overview/gpa_calc.py
@@ -3,7 +3,7 @@ print("Please enter all your letter grades, one per line.")
 print("Enter a blank line to designate the end.")
 
 points = {
-  "A": 4.0,
+  "A+": 4.0,
   "A": 4.0,
   "A-": 3.67,
   "B+": 3.33,
@@ -11,7 +11,8 @@ points = {
   "B-": 2.67,
   "C+": 2.33,
   "C": 2.0,
-  "C": 1.67,
+  "C-": 1.67,
+  "D+": 1.33,
   "D": 1.0,
   "F": 0.0
 }
